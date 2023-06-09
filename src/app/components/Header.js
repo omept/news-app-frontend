7aed327fca69daa9a3cdf8e1b6f2e3b3af409382
George Onwuasoanya <georgetheprogrammer@gmail.com>
2023-06-09 23:33:48 +0100

    template scaffolded

---
+++ src/app/components/Header.js
@@ -0,0 +1,90 @@
+import "tw-elements/dist/css/tw-elements.min.css";
+export default function Header() {
+  return (
+    <nav
+      className="flex-no-wrap relative flex w-full items-center justify-between bg-neutral-100 py-2 shadow-md shadow-black/5 dark:bg-neutral-600 dark:shadow-black/10 lg:flex-wrap lg:justify-start lg:py-4"
+      data-te-navbar-ref
+    >
+      <div className="flex w-full flex-wrap items-center justify-between px-3">
+        <button
+          className="block border-0 bg-transparent px-2 text-neutral-500 hover:no-underline hover:shadow-none focus:no-underline focus:shadow-none focus:outline-none focus:ring-0 dark:text-neutral-200 lg:hidden"
+          type="button"
+          data-te-collapse-init
+          data-te-target="#navbarSupportedContent1"
+          aria-controls="navbarSupportedContent1"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="[&>svg]:w-7">
+           
+            <h1 className="font-bold ">NewsFeed App</h1>
+          </span>
+        </button>
+
+        <div
+          className="!visible hidden flex-grow basis-[100%] items-center lg:!flex lg:basis-auto"
+          id="navbarSupportedContent1"
+          data-te-collapse-item
+        >
+          <a
+            className="mb-4 mr-2 flex items-center text-neutral-900 hover:text-neutral-900 focus:text-neutral-900 dark:text-neutral-200 dark:hover:text-neutral-400 dark:focus:text-neutral-400 lg:mb-0 lg:mt-0"
+            href="/"
+          >
+            <h1 className="font-bold ">NewsFeed App</h1>
+          </a>
+        </div>
+
+        <div className="relative flex items-center">
+          <div className="relative">
+            <a
+              className="hidden-arrow mr-4 flex items-center text-neutral-500 hover:text-neutral-700 focus:text-neutral-700 disabled:text-black/30 dark:text-neutral-200 dark:hover:text-neutral-300 dark:focus:text-neutral-300 [&.active]:text-black/90 dark:[&.active]:text-neutral-400"
+              href="/login"
+              id="dropdownMenuButton1"
+              role="button"
+              data-te-dropdown-toggle-ref
+              aria-expanded="false"
+            >
+              Login
+            </a>
+          </div>{" "}
+          <div className="relative">
+            <a
+              className="hidden-arrow mr-4 flex items-center text-neutral-500 hover:text-neutral-700 focus:text-neutral-700 disabled:text-black/30 dark:text-neutral-200 dark:hover:text-neutral-300 dark:focus:text-neutral-300 [&.active]:text-black/90 dark:[&.active]:text-neutral-400"
+              href="/sign-up"
+              id="dropdownMenuButton1"
+              role="button"
+              data-te-dropdown-toggle-ref
+              aria-expanded="false"
+            >
+              Sign up
+            </a>
+          </div>
+          <div className="relative">
+            <a
+              className="hidden-arrow mr-4 flex items-center text-neutral-500 hover:text-neutral-700 focus:text-neutral-700 disabled:text-black/30 dark:text-neutral-200 dark:hover:text-neutral-300 dark:focus:text-neutral-300 [&.active]:text-black/90 dark:[&.active]:text-neutral-400"
+              href="/logout"
+              id="dropdownMenuButton1"
+              role="button"
+              data-te-dropdown-toggle-ref
+              aria-expanded="false"
+            >
+              Logout
+            </a>
+          </div>
+          <div className="relative">
+            <a
+              className="hidden-arrow flex items-center whitespace-nowrap transition duration-150 ease-in-out motion-reduce:transition-none"
+              href="/settings"
+              id="dropdownMenuButton2"
+              role="button"
+              data-te-dropdown-toggle-ref
+              aria-expanded="false"
+            >
+              Settings
+            </a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
--- src/app/login/page.js
+++ src/app/login/page.js
@@ -14,7 +14,7 @@ export default function Home() {
         <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-              Create and account
+              Login
             </h1>
             <form className="space-y-4 md:space-y-6" action="#">
               <div>
@@ -49,50 +49,10 @@ export default function Home() {
                   required=""
                 />
               </div>
-              <div>
-                <label
-                  // for="confirm-password"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Confirm password
-                </label>
-                <input
-                  type="confirm-password"
-                  name="confirm-password"
-                  id="confirm-password"
-                  placeholder="••••••••"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  required=""
-                />
-              </div>
-              <div className="flex items-start">
-                <div className="flex items-center h-5">
-                  <input
-                    id="terms"
-                    aria-describedby="terms"
-                    type="checkbox"
-                    className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
-                    required=""
-                  />
-                </div>
-                <div className="ml-3 text-sm">
-                  <label
-                    //   for="terms"
-                    className="font-light text-gray-500 dark:text-gray-300"
-                  >
-                    I accept the{" "}
-                    <a
-                      className="font-medium text-primary-600 hover:underline dark:text-primary-500"
-                      href="#"
-                    >
-                      Terms and Conditions
-                    </a>
-                  </label>
-                </div>
-              </div>
+
               <button
                 type="submit"
-                style={{background: "red"}}
+                style={{ background: "red" }}
                 className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
               >
                 Create an account
@@ -104,6 +64,15 @@ export default function Home() {
                   className="font-medium text-primary-600 hover:underline dark:text-primary-500"
                 >
                   Login here
+                </a>
+              </p>
+              <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                {" =>> "}
+                <a
+                  href="#"
+                  className="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                >
+                  Home
                 </a>
               </p>
             </form>
